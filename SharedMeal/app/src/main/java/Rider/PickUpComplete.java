83cdef7b211b5afe765d2135ba9183fa38526c03
Vamsi Konakanchi <vmskonakanchi@gmail.com>
2022-03-08 19:43:46 +0530

    added ridercomplete activity

---
+++ SharedMeal/app/src/main/java/Rider/PickUpComplete.java
@@ -0,0 +1,23 @@
+package Rider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.sharedmeal.R;
+
+public class PickUpComplete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pick_up_complete);
+    }
+
+    public void CompletePickUp(View view) {
+        //called when complete pick up button is clicked
+        //todo: crate a page where you can upload photo of food donation with submit button
+
+    }
+}
